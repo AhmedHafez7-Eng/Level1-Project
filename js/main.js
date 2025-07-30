c8428b68d81fb693e62d201e75e71c9a61614f7e
AhmedHafez7-Eng <ahmedhafezoffic@gmail.com>
2025-07-30 23:28:36 +0300

    Handling Slider Responsivity

--- js/main.js
+++ js/main.js
@@ -1,21 +1,32 @@
 // By: Ahmed Hafez
 
 /* ============================================= */
-// Swiper Testimonial Slider
-const swiper = new Swiper('.mySwiper', {
+const swiper = new Swiper(".mySwiper", {
     loop: true,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
     },
     pagination: {
-        el: '.swiper-pagination',
+        el: ".swiper-pagination",
         clickable: true
     },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1 // Small screens
+        },
+        640: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 1
+        },
+        1024: {
+            slidesPerView: 2
+        }
+    }
 });
+
 
 /* ============================================= */
