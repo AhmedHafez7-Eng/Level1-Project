f1c5095b72b547d35c4dafd9b057dc76dd35f275
AhmedHafez7-Eng <ahmedhafezoffic@gmail.com>
2025-07-30 23:00:53 +0300

    Adding About & Testimonial Sections

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// By: Ahmed Hafez
+
+/* ============================================= */
+// Swiper Testimonial Slider
+const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+});
+
+/* ============================================= */
